ddeaf84dab609dc3bf253d674378b53bd1bcc4fa
Randy Cullins <randycullins2018@gmail.com>
2022-11-06 20:29:04 -0700

    added functionailty for to do list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoItems, setTodoItems] = useState([]);
+
+  const addItems = (e) => {
+    e.preventDefault();
+
+    if (!inputValue) {
+      alert("Enter an input");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: inputValue,
+    };
+
+    setTodoItems((prev) => [...prev, item]);
+    setInputValue("");
+  };
+
+  const deleteItems = (id) => {
+    const newArray = todoItems.filter((item) => item.id !== id);
+    setTodoItems(newArray);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>To Do List</h1>
+          <form onSubmit={addItems}>
+            <input
+              id="Todo"
+              name="Todo"
+              placeholder="Text here"
+              value={inputValue || ""}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button type="submit">Add Todo Item</button>
+          </form>
+
+          <ul>
+            {todoItems.map((item) => (
+              <li key={item.id}>
+                {item.value}
+                <button onClick={() => deleteItems(item.id)}>x</button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
